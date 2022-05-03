298c5e6e4659153a29f8d2041523a01a166c71ba
Anish <anishkakaiya@outlook.com>
2022-05-03 16:45:55 +0100

    rearrange html to be in index.erb

--- app.rb
+++ app.rb
@@ -1,11 +1,6 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 
-
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-     <img src='http://placekitten.com/500/500'>
-   </div>"
+  erb(:index)
 end
-
-

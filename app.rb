07dff9e06a6d256a7af8d8338d289c2ae3d4023e
George Dainton <gcdainton94@gmail.com>
2022-05-03 16:15:11 +0100

    Added cat picture with  red border to /cat path

--- app.rb
+++ app.rb
@@ -1,23 +1,11 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 
-get '/' do 
-  'Hello world'
-end 
-get '/secret' do
-  'Hello Anish!'
-  'It\'s me, George'
-end
-
-get '/members' do
-  'Welcome to the VIP lounge'
-end
 
 get '/cat' do
-  "<div>
-  <img src='https://i.imgur.com/jFaSxym.png'>
-  </div>"
-end 
-
+  "<div style='border: 3px dashed red'>
+     <img src='http://placekitten.com/500/500'>
+   </div>"
+end
 
 
